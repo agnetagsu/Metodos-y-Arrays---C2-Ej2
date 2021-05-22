c04480f8851e4f8a530d3678e8e70685561f69ed
Agneta <agnetagsu@yahoo.es>
2021-05-23 00:50:20 +0200

    Agregar archivos de proyecto.

---
+++ Metodos y Arrays - C2 Ej2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Metodos_y_Arrays___C2_Ej2
+{
+    class Mostrarnumerosaleatorios
+    {
+        public int Rango()
+        {
+            int a, z;
+            Random aleatorio = new Random();
+            Console.WriteLine("Desde que numero: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hasta que numero: ");
+            z = Convert.ToInt32(Console.ReadLine());
+            return aleatorio.Next(a,z);
+        }
+        static void Main(string[] args)
+        {
+            Mostrarnumerosaleatorios numero = new Mostrarnumerosaleatorios();
+            Console.WriteLine(numero.Rango());
+        }
+    }
+}
